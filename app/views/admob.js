15983b9bbad539e0718168a2193867ae8389f9f3
kf <7kfpun@gmail.com>
2017-01-16 23:36:51 +0800

    Refresh ad banner

--- app/views/admob.js
+++ app/views/admob.js
@@ -8,6 +8,7 @@ import {
 
 // 3rd party libraries
 import { AdMobBanner } from 'react-native-admob';
+import timer from 'react-native-timer';
 
 import { config } from '../config';
 
@@ -17,22 +18,39 @@ const styles = StyleSheet.create({
   },
 });
 
-function AdmobCell(props) {
-  return (
-    <View style={[styles.container, { margin: props.margin }]}>
-      <AdMobBanner bannerSize={props.bannerSize} adUnitID={config.admob[Platform.OS].banner} />
-    </View>
-  );
+export default class AdmobCell extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentDidMount() {
+    timer.clearTimeout(this);
+    timer.setInterval(this, 'adRefresh', () => this.setState({ adRefresh: Math.random() }), this.props.refreshInterval);
+  }
+
+  componentWillUnmount() {
+    timer.clearInterval(this);
+  }
+
+  render() {
+    return (
+      <View style={[styles.container, { margin: this.props.margin }]}>
+        <AdMobBanner key={this.state.adRefresh} bannerSize={this.props.bannerSize} adUnitID={config.admob[Platform.OS].banner} />
+      </View>
+    );
+  }
 }
 
 AdmobCell.propTypes = {
   margin: React.PropTypes.number,
   bannerSize: React.PropTypes.string,
+  refreshInterval: React.PropTypes.number,
 };
 
 AdmobCell.defaultProps = {
   margin: 0,
   bannerSize: 'smartBannerPortrait',
+  refreshInterval: 10000,
 };
-
-module.exports = AdmobCell;
